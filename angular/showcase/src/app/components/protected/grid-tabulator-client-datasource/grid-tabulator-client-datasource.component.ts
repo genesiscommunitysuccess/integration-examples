4fd269a6bd9b08f5b8171eea9093dbeb7e978029
Artur Krasinski <artur@minion.pl>
2023-12-01 15:03:54 +0100

    feat: wip FUI-1542

--- angular/showcase/src/app/components/protected/grid-tabulator-client-datasource/grid-tabulator-client-datasource.component.ts
+++ angular/showcase/src/app/components/protected/grid-tabulator-client-datasource/grid-tabulator-client-datasource.component.ts
@@ -1,5 +1,8 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ViewChildren, AfterViewInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DatasourceDefaults } from '@genesislcap/foundation-comms';
+import { DI } from '@microsoft/fast-foundation';
+import { Connect } from '@genesislcap/foundation-comms';
 
 @Component({
   selector: 'app-grid-tabulator-client-datasource',
@@ -10,5 +13,19 @@ import { CommonModule } from '@angular/common';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class GridTabulatorClientDatasourceComponent {
+  @ViewChild('dataSource') dataSourceElement!: any;
 
+  resourceName = 'ALL_COUNTERPARTYS'
+  maxView = DatasourceDefaults.MAX_VIEW_1000;
+  maxRows = DatasourceDefaults.MAX_ROWS_250
+  criteria = 'NAME != null';
+  restartOnReconnection = true;
+  displayZero = false;
+
+  fixConnect() {
+    
+  const container = DI.getOrCreateDOMContainer();
+  const connect: Connect = container.get(Connect);
+  this.displayZero = true
+  }
 }
