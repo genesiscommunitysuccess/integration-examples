95139500b9a76718a53684ec6f4088c6ac7a71a8
Artur Krasinski <artur@minion.pl>
2023-12-07 17:04:16 +0100

    feat: wip FUI-1542

--- angular/showcase/src/app/components/protected/grid-tabulator-client-datasource/grid-tabulator-client-datasource.component.ts
+++ angular/showcase/src/app/components/protected/grid-tabulator-client-datasource/grid-tabulator-client-datasource.component.ts
@@ -1,8 +1,13 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild, Input, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatasourceDefaults } from '@genesislcap/foundation-comms';
 import { DEFAULT_CRITERIA, DEFAULT_RESOURCE_NAME } from '../../../services/store.service';
-
+import {
+  setComponentItemsMap,
+  getElementByTagFromComponent,
+} from '../../../utils/goldenLayout.helper';
+import { LayoutEmitEvents } from '@genesislcap/foundation-layout';
+import { LayoutComponentsNames } from './grid-tabulator-client-datasource.types';
 @Component({
   selector: 'app-grid-tabulator-client-datasource',
   standalone: true,
@@ -11,33 +16,53 @@ import { DEFAULT_CRITERIA, DEFAULT_RESOURCE_NAME } from '../../../services/store
   styleUrl: './grid-tabulator-client-datasource.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class GridTabulatorClientDatasourceComponent implements OnChanges {
-  @ViewChild('datasource1') datasource1Element!: any;
-  @ViewChild('datasource2') datasource2Element!: any;
+export class GridTabulatorClientDatasourceComponent implements AfterViewInit, OnChanges, OnDestroy {
+  @ViewChild('gridLayout') gridLayoutElement!: any;
 
   @Input() resourceName: string = DEFAULT_RESOURCE_NAME;
   @Input() criteria: string = DEFAULT_CRITERIA;
 
+  layoutComponentsMap: Map<LayoutComponentsNames, any> = new Map();
   maxView = DatasourceDefaults.MAX_VIEW_1000;
   maxRows = DatasourceDefaults.MAX_ROWS_250;
   restartOnReconnection = true;
-  displayZero = false;
+  ready = false
+  listeners: Function[] = [];
 
-  
+  setReady() {
+    this.ready = true;
+  }
+
+  ngAfterViewInit() {
+    setComponentItemsMap(this.gridLayoutElement.nativeElement, this.layoutComponentsMap);
+    const listner = this.setReady.bind(this)
+    this.listeners.push(listner);
+    this.gridLayoutElement.nativeElement.addEventListener(LayoutEmitEvents.firstLoaded, listner);
+  }
+
   ngOnChanges(changes: SimpleChanges) {
-    if (!changes['resourceName'] && !changes['criteria']) {
+    if (!changes['resourceName'] && !changes['criteria'] && !this.ready) {
       return;
     }
 
-    console.log({ changes })
+    if (this.layoutComponentsMap.size > 0) {
+      this.layoutComponentsMap.forEach((component: any) => {
+        const datasource = getElementByTagFromComponent(component, 'grid-tabulator-client-side-datasource');
+        
+        if (changes['resourceName']) {
+          datasource.resourceName = changes['resourceName'].currentValue;
+        }
 
-    if (changes['resourceName']) {
-      this.datasource1Element.nativeElement.resourceName = changes['resourceName'].currentValue;
-      this.datasource2Element.nativeElement.resourceName = changes['resourceName'].currentValue;
+        if (changes['criteria']) {
+          datasource.criteria = changes['criteria'].currentValue;
+        }
+      });
     }
-    if (changes['criteria']) {
-      this.datasource1Element.nativeElement.resourceName = changes['resourceName'].currentValue;
-      this.datasource2Element.nativeElement.resourceName = changes['resourceName'].currentValue;
-    }
+  }
+
+  ngOnDestroy() {
+    this.listeners.forEach((listener) => {
+      this.gridLayoutElement.nativeElement.removeEventListener(LayoutEmitEvents.firstLoaded, listener);
+    });
   }
 }
