d4977cd703787d40f841774537c6214a05f5a673
michal.skawianczyk <michal.skawianczyk@genesis.global>
2024-02-05 12:13:00 +0100

    wip

--- angular/blank-app-seed/src/app/pages/protected/protected.component.ts
+++ angular/blank-app-seed/src/app/pages/protected/protected.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-protected',
   standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './protected.component.html',
   styleUrls: ['./protected.component.css'],
 })
 export class ProtectedComponent {
-  
+
 }
