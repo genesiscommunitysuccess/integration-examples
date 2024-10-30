e808efef2ee72c1b056a0b431abc3e1ad404a5db
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-10-30 11:51:43 +0100

    feat: add simple react 19 example

--- react-19/rapid/src/App.js
+++ react-19/rapid/src/App.js
@@ -6,6 +6,7 @@ import {
   rapidButton,
   rapidTextField,
   rapidCheckbox,
+  rapidFlyout,
 } from '@genesislcap/rapid-design-system';
 
 provideDesignSystem()
@@ -13,7 +14,8 @@ provideDesignSystem()
     rapidCard(),
     rapidButton(),
     rapidTextField(),
-    rapidCheckbox()
+    rapidCheckbox(),
+    rapidFlyout(),
   );
 
 
@@ -21,6 +23,7 @@ function App() {
   // State to hold the input value
   const [inputValue, setInputValue] = useState('');
   const [checkboxValue, setCheckboxState] = useState(false);
+  const [flyoutState, setFlyoutState] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -30,22 +33,42 @@ function App() {
     setCheckboxState(!!event.target.checked);
   };
 
+  const showFlyout = () => {
+    setFlyoutState(true);
+  };
+
+  const hideFlyout = () => {
+    setFlyoutState(false);
+  };
+
+
   return (
-    <rapid-card>
-      <h2>Genesis Foundation React</h2>
-      <p>input value: {inputValue} / checkbox value: {checkboxValue ? 'true' : 'false'}</p>
-      <div style={{ padding: '10px', display: 'flex', 'justify-content': 'space-between', 'vertical-align': 'middle', 'align-items': 'center'}}>
-      <rapid-text-field
-        name='exampleTextField'
-        placeholder="Enter Some Text"
-        value={inputValue} 
-        onInput={handleInputChange}
-        style={{ 'margin': ' 0 50px 0 0' }}
-      ></rapid-text-field>
-      <rapid-checkbox onChange={handleCheckboxChange}></rapid-checkbox>
-      </div>
-      <rapid-button appearance="accent" onClick={() => console.log(inputValue)}>Click Me</rapid-button>
-  </rapid-card>
+    <section>
+      <rapid-card>
+        <h2>Genesis Foundation React</h2>
+        <p>input value: {inputValue} / checkbox value: {checkboxValue ? 'true' : 'false'}</p>
+        <div style={{ padding: '10px', display: 'flex', 'justify-content': 'space-between', 'vertical-align': 'middle', 'align-items': 'center'}}>
+        <rapid-text-field
+          name='exampleTextField'
+          placeholder="Enter Some Text"
+          value={inputValue} 
+          onInput={handleInputChange}
+          style={{ 'margin': ' 0 50px 0 0' }}
+        ></rapid-text-field>
+        <rapid-checkbox onChange={handleCheckboxChange}></rapid-checkbox>
+        </div>
+        <rapid-button appearance="accent" onClick={() => console.log(inputValue)}>Click Me</rapid-button>
+
+        <rapid-button appearance="accent" onClick={showFlyout}>Show flayout</rapid-button>
+      </rapid-card>
+      <rapid-flyout
+        position="right"
+        closed={!flyoutState}
+        onclosed={hideFlyout}
+      >
+        Flyout content
+      </rapid-flyout>
+    </section>
   );
 }
 
